932dd98b0579d008e6640a7ffd2e9ff53064ff26
ThisUsernameIsCool <grantreddickgames@gmail.com>
2020-06-01 14:11:54 -0400

    made changes

---
+++ server.py
@@ -0,0 +1,18 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('localhost', 1234))
+s.listen(5)
+
+
+def main():
+    while True:
+        clientsocket, address = s.accept()
+        while address != None:
+            try:
+                message = input("Enter Message: ")
+                messageb = message.encode("utf-8")
+                clientsocket.send(messageb)
+            except:
+                print("Error: Client Disconnected")
+main()
